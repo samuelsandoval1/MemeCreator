fe584860a74999422ebe9554056d66c159dd1198
Samuel Sandoval <sjsandoval1000@gmail.com>
2021-03-15 19:08:03 -0700

    😎 finished initial part

--- script.js
+++ script.js
@@ -6,60 +6,54 @@ function generateMeme(img, topText, bottomText, topTextSize, bottomTextSize) {
     canvas.height = img.height;
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
     ctx.drawImage(img, 0, 0);
-    
+
     ctx.fillStyle = 'white';
     ctx.strokeStyle = 'black';
     ctx.textAlign = 'center';
-    
-    let fontSize = canvas.width * topTextSize; //Font Size will change based on our input sliders
-    ctx.font = `${fontSize}px Impact`; // We'll be using Impact font, which is used by most memes
-    ctx.lineWidth = fontSize / 20; 
-    
+
+    let fontSize = canvas.width * topTextSize;
+    ctx.font = `${fontSize}px Impact`;
+    ctx.lineWidth = fontSize / 20;
+
     ctx.textBaseline = 'top';
-  topText.split('\n').forEach((t, i) => {
-    ctx.fillText(t, canvas.width / 2, i * fontSize, canvas.width);
-    ctx.strokeText(t, canvas.width / 2, i * fontSize, canvas.width);
-  });
+    topText.split('\n').forEach((t, i) => {
+        ctx.fillText(t, canvas.width / 2, i * fontSize, canvas.width);
+        ctx.strokeText(t, canvas.width / 2, i * fontSize, canvas.width);
+    });
 
-  // Bottom text font size
-  fontSize = canvas.width * bottomTextSize;
-  ctx.font = `${fontSize}px Impact`;
-  ctx.lineWidth = fontSize / 20;
+    fontSize = canvas.width * bottomTextSize;
+    ctx.font = `${fontSize}px Impact`;
+    ctx.lineWidth = fontSize / 20;
 
-  // Draw bottom text
-  ctx.textBaseline = 'bottom';
-  bottomText.split('\n').reverse().forEach((t, i) => { // .reverse() because it's drawing the bottom text from the bottom up
-    ctx.fillText(t, canvas.width / 2, canvas.height - i * fontSize, canvas.width);
-    ctx.strokeText(t, canvas.width / 2, canvas.height - i * fontSize, canvas.width);
-  });
+    ctx.textBaseline = 'bottom';
+    bottomText.split('\n').reverse().forEach((t, i) => { // .reverse() because it's drawing the bottom text from the bottom up
+        ctx.fillText(t, canvas.width / 2, canvas.height - i * fontSize, canvas.width);
+        ctx.strokeText(t, canvas.width / 2, canvas.height - i * fontSize, canvas.width);
+    });
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-    // Initialize variables
+window.addEventListener('DOMContentLoaded', (event) => {
+
     const topTextInput = document.getElementById('top-text');
     const bottomTextInput = document.getElementById('bottom-text');
     const topTextSizeInput = document.getElementById('top-text-size-input');
     const bottomTextSizeInput = document.getElementById('bottom-text-size-input');
     const imageInput = document.getElementById('image-input');
     const generateBtn = document.getElementById('generate-btn');
-    // Default/Demo text
-    topTextInput.value = 'Top\nValue';
-    bottomTextInput.value = 'Bottom\nValue';
-  
-    // Generate button click listener
+
+
+
     generateBtn.addEventListener('click', () => {
-      // Read image as DataURL using the FileReader API
-      const reader = new FileReader();
-      reader.onload = () => {
-        const img = new Image();
-        img.src = reader.result;
-        img.onload = () => {
-          generateMeme(img, topTextInput.value, bottomTextInput.value, topTextSizeInput.value, bottomTextSizeInput.value);
+        // Read image as DataURL using the FileReader API
+        const reader = new FileReader();
+        reader.onload = () => {
+            const img = new Image;
+            img.src = reader.result;
+            img.onload = () => {
+                generateMeme(img, topTextInput.value, bottomTextInput.value,topTextSizeInput.value, bottomTextSizeInput.value);
+            };
         };
-      };
-      reader.readAsDataURL(imageInput.files[0]);
+        reader.readAsDataURL(imageInput.files[0]);
     });
-  });
-  
+});
